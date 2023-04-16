bff6d72dcfc38006f87193d05ae380a0426f25e6
akashharde001 <120962312+akashharde001@users.noreply.github.com>
2023-04-16 17:44:50 +0530

    Update streamlight.py

--- streamlight.py
+++ streamlight.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('hello snowflake')
-
+streamlit.title('Healty chichen....')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('🥣Breakfast Menu')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
